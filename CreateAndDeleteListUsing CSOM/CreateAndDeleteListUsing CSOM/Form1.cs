036986bf1ec497d70a89e5fb57864cfa04195fc9
Parag Yeole <50729546+ParagYeole@users.noreply.github.com>
2021-07-12 16:12:54 +0530

    Add files via upload

---
+++ CreateAndDeleteListUsing CSOM/CreateAndDeleteListUsing CSOM/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SPClient=Microsoft.SharePoint.Client;
+
+namespace CreateAndDeleteListUsing_CSOM
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string url = "http://appsemea.sanofi.com/ws/PVI_DEV/";
+            SPClient.ClientContext ctx = new SPClient.ClientContext(url);
+            SPClient.Web web = ctx.Web;
+
+            SPClient.ListCreationInformation ListInfo = new SPClient.ListCreationInformation();
+            ListInfo.Title = "Parag_TEST1";
+            ListInfo.Description = "Created for TESTing purpose";
+            ListInfo.TemplateType = (int)SPClient.ListTemplateType.GenericList;
+            SPClient.List list = web.Lists.Add(ListInfo);
+            ctx.ExecuteQuery();
+            MessageBox.Show("List Created");
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string url = "http://appsemea.sanofi.com/ws/PVI_DEV/";
+            SPClient.ClientContext ctx = new SPClient.ClientContext(url);
+            SPClient.Web web = ctx.Web;
+            SPClient.List list= web.Lists.GetByTitle("Parag_TEST1");
+            list.DeleteObject();
+            ctx.ExecuteQuery();
+            MessageBox.Show("List Deleted");
+        }
+    }
+}
